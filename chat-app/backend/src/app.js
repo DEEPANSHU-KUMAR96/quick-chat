5face750e23a858c01dc1dbc4f1d0589bfef5fc4
Deepanshu <deepanshukumar9650@gmail.com>
2026-07-12 15:50:22 +0530

    feat: initialize express server with socket.io support and configured message routes

--- chat-app/backend/src/app.js
+++ chat-app/backend/src/app.js
@@ -11,7 +11,10 @@ import registerSocketHandlers from "./sockets/socketHandler.js";
   const app = express();
 
   app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+      "https://quick-chat-6meb.onrender.com"
+    ],
+
     methods:["GET","POST"],
     credentials:true
   }));
@@ -36,7 +39,9 @@ import registerSocketHandlers from "./sockets/socketHandler.js";
   const server = http.createServer(app);
 
   const io = new Server(server, {
-    cors: { origin:"http://localhost:5173",
+    cors: { origin:["http://localhost:5173",
+      "https://quick-chat-6meb.onrender.com"
+    ],
      methods:["GET","POST"],
      credentials:true },
   });
